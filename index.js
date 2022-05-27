a47e4f22f52a99cf1784cc9ae527b66b8fe6b1fe
ajhar10 <ajharrimon90@gmail.com>
2022-05-28 00:54:09 +0600

    add product

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lp7y4.mongodb.net/?retryWrites=true&w=majority`;
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('nextShop').collection('products');
+        //next
+        const ordersCollection = client.db('nextShop').collection('orders');
+        const userCollection = client.db('nextShop').collection('users');
+        const reviewsCollection = client.db('nextShop').collection('reviews');
 
         // all products
         app.get('/products', async (req, res) => {
@@ -31,6 +49,13 @@ async function run() {
             res.send(products);
         });
 
+        // add product
+        app.post('/addProduct', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
         // get a product
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -38,6 +63,8 @@ async function run() {
             const result = await productsCollection.findOne(query)
             res.send(result);
         })
+
+
     }
     finally {
 
